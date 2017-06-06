f31f901a512c26e8a3e5b941f3695078e2625dd8
Maksim <m.glinka@rambler-co.ru>
2017-06-06 17:16:45 +0300

    Adds merge sort

--- sort_types/sort_types.rb
+++ sort_types/sort_types.rb
@@ -19,11 +19,51 @@ class SortTypes
     arr
   end
 
-
   def insertion(arr)
+    arr.each.with_index do |_, ind|
+      key = arr[ind]
+      j = ind - 1
+
+      while j >= 0 && arr[j] > key
+        arr[j + 1] = arr[j]
+        j -= 1
+      end
+      arr[j + 1] = key
+    end
   end
 
-  def merge(arr)
+  def merge_sort(arr, left, right)
+    return if left >= right
+    # binding.pry
+    #binding.pry if right - left == 1 && arr[left] > arr[right]
+    # return change(arr, left, right)[left..right] if right - left == 1 && arr[left] > arr[right]
+    middle = (left + right) / 2
+    merge_sort(arr, left, middle)
+    merge_sort(arr, middle + 1, right)
+    merge(arr, left, middle, right)
+  end
+
+  def merge(arr, left, middle, right)
+    b = []
+    c = []
+    n1 = middle - left + 1
+    n2 = right - middle
+    b[n1 + 1] = 100_500
+    c[n2 + 1] = 100_500
+    b[0..-2] = arr[left..middle]
+    c[0..-2] = arr[(middle + 1)..right]
+    i = 0
+    j = 0
+    (left..right).each do |k|
+      if b[i] <= c[j]
+        arr[k] = b[i]
+        i += 1
+      else
+        arr[k] = c[j]
+        j += 1
+      end
+    end
+    arr
   end
 
   def quick(arr)
--- spec/sort_types/sort_types_spec.rb
+++ spec/sort_types/sort_types_spec.rb
@@ -10,16 +10,16 @@ RSpec.describe SortTypes do
   end
 
   context '#insertion' do
-    arr = [4, 5, 3, 1, 2, 6, 7]
+    arr = [7, 10, 3, 1, 5, 6, 7]
     it 'sorts the array' do
-      expect(subject.insertion(arr)).to eq [1, 2, 3, 4, 5, 6, 7]
+      expect(subject.insertion(arr)).to eq [1, 3, 5, 6, 7, 7, 10]
     end
   end
 
-  context '#merge' do
+  context '#merge_sort' do
     arr = [4, 5, 3, 1, 2, 6, 7]
     it 'sorts the array' do
-      expect(subject.merge(arr)).to eq [1, 2, 3, 4, 5, 6, 7]
+      expect(subject.merge_sort(arr, 0, 6)).to eq [1, 2, 3, 4, 5, 6, 7]
     end
   end
 
